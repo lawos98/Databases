e8bb6da5dd501147a7488384331c7d9d7bf82b12
lawos98 <85324038+lawos98@users.noreply.github.com>
2021-10-26 10:46:35 +0200

    Update exercises_2-3.sql

--- L_2/Home/exercises_2-3.sql
+++ L_2/Home/exercises_2-3.sql
@@ -1,4 +1,4 @@
 /* Podaj nr zamówienia oraz wartość zamówienia, dla zamówień, dla których
 łączna liczba zamawianych jednostek produktów jest > 250 */
 
-Select OrderID,Sum(UnitPrice) from [Order Details] group by OrderID having Sum(Quantity)>250
+Select OrderID,Sum(CONVERT(money,UnitPrice*Quantity*(1-Discount))) from [Order Details] group by OrderID having Sum(Quantity)>250
